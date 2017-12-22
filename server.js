b76b5161d3dbdc2b9e619d3652b9eb9d1478f7b1
Christopher Olaussen <christopher.olaussen@gmail.com>
2017-12-22 10:28:39 +0100

    fix con bug

--- server.js
+++ server.js
@@ -56,12 +56,12 @@ function testDb() {
 	  		console.log("Connected to database!");
 		});
 
-		connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+		con.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 		  if (error) throw error;
 		  console.log('The solution is: ', results[0].solution);
 		});
  
-		connection.end();
+		con.end();
 	}
 }
 
