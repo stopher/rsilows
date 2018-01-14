074483e1ab6e843f924c8b033bae31291c6bc45b
Christopher Olaussen <christopher.olaussen@gmail.com>
2018-01-14 15:54:35 +0100

    test

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ const tickersSchedule = schedule.scheduleJob('* * 18 * * *', function(fireDate){
 });
 */
 
-const rsiSchedule = schedule.scheduleJob('* 10 1 * * *', function(fireDate){
+const rsiSchedule = schedule.scheduleJob('10 18 * * *', function(fireDate){
   console.log('This rsi schedule was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
   getFreshOseRSIs();
 });
